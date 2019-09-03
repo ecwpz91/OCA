eeb4ec4d9ba48406a8e57d5d5bd9fe0498d50bd6
ecwpz91 <ecwpz91@users.noreply.github.com>
2019-09-03 14:14:40 -0400

    Initial revision

---
+++ chapter1/src/book/Goo.java
@@ -0,0 +1,10 @@
+package book;
+
+import cert.*;
+
+class Goo {
+	public static void main(String[] args) {
+		Sludge o = new Sludge();
+		o.testIt();
+	}
+}
---
+++ chapter1/src/notcert/Cloo.java
@@ -0,0 +1,14 @@
+package notcert;
+
+import cert.Roo;
+
+class Cloo extends Roo {
+	public static void main(String[] args) {
+		Cloo c = new Cloo();
+		c.testCloo();
+	}
+
+	public void testCloo() {
+		System.out.println(doRooThings());
+	}
+}
