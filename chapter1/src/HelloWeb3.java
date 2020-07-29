14bd1d24206e3b91a203705052669746d84369f0
ecwpz91 <ecwpz91@users.noreply.github.com>
2020-07-29 06:36:50 -0400

    Initial revision

---
+++ chapter1/src/HelloWeb3.java
@@ -0,0 +1,49 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+public class HelloWeb3 extends Applet implements MouseMotionListener, ActionListener {
+	int messageX = 125, messageY = 95;
+	String theMessage;
+	Button theButton;
+	int colorIndex = 0;
+	static Color[] someColors = {Color.black, Color.red, Color.green, Color.blue, Color.magenta};
+
+	public void init() {
+		theMessage = getParameter("message");
+		theButton = new Button("change Color");
+		add(theButton);
+		addMouseMotionListener(this);
+		theButton.addActionListener(this);
+	}
+
+	public void paint( Graphics graphics ) {
+		graphics.drawString( theMessage, messageX, messageY );
+	}
+
+	public void mouseDragged( MouseEvent e ) {
+		messageX = e.getX();
+		messageY = e.getY();
+		repaint();
+	}
+
+	public void mouseMoved( MouseEvent e ) { }
+
+	public void actionPerformed( ActionEvent e ) {
+		if ( e.getSource() == theButton ) {
+			changeColor();
+		}
+	}
+
+	synchronized private void changeColor() {
+		if ( ++colorIndex == someColors.length )
+			colorIndex = 0;
+		setForeground( currentColor() );
+		repaint();
+	}
+
+	synchronized private Color currentColor() {
+		return someColors[ colorIndex ];
+	}
+
+}
