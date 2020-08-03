22f113880c6b88f3be0a42a1fd8c733785991bc1
ecwpz91 <ecwpz91@users.noreply.github.com>
2020-08-03 07:03:00 -0400

    Added commentary on synchronized Java keyword

--- chapter1/src/HelloWeb3.java
+++ chapter1/src/HelloWeb3.java
@@ -38,6 +38,16 @@ public class HelloWeb3 extends Applet implements MouseMotionListener, ActionList
 		}
 	}
 
+	// The synchronied keyword has to do with threads and indicates that these two methods 
+	// can never be running at the same time. They must always run one after the other.
+	//
+	// The reason for this is that in changeColor() we increment colorIndex befroe testing
+	// its value. That means that for some brief period of time while Java is running
+	// through our code, colorIndex can have a value that is past the end of our array. If our
+	// currentColor() method happened to run at that same moment, we would see a runtime 
+	// "array out of bounds" error. This is an example of Java making it easy to deal
+	// with this type of problem easily without having to think about ways we could fudge around
+	// how to deal with this in a if/then/case scenario.
 	synchronized private void changeColor() {
 		if ( ++colorIndex == someColors.length )
 			colorIndex = 0;
