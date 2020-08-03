70e8c9bd2c89980e5ba55cdf5fe4ecdfc9455a68
ecwpz91 <ecwpz91@users.noreply.github.com>
2020-08-03 07:19:39 -0400

    Initial revision

---
+++ chapter1/src/HelloWeb4.java
@@ -0,0 +1,77 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+public class HelloWeb4 extends Applet implements MouseMotionListener, ActionListener, Runnable {
+	int messageX = 125, messageY = 95;
+	String theMessage;
+	Button theButton;
+	int colorIndex = 0;
+	static Color[] someColors = {Color.black, Color.red, Color.green, Color.blue, Color.magenta};
+	Thread blinkThread;
+	boolean blinkState;
+
+	public void init() {
+		theMessage = getParameter("message");
+		theButton = new Button("change Color");
+		add(theButton);
+		addMouseMotionListener(this);
+		theButton.addActionListener(this);
+	}
+
+	public void paint( Graphics graphics ) {
+		graphics.setColor( blinkState ? Color.white : currentColor() );
+		graphics.drawString( theMessage, messageX, messageY );
+	}
+
+	public void mouseDragged( MouseEvent e ) {
+		messageX = e.getX();
+		messageY = e.getY();
+		repaint();
+	}
+
+	public void mouseMoved( MouseEvent e ) { }
+
+	public void actionPerformed( ActionEvent e ) {
+		if ( e.getSource() == theButton ) {
+			changeColor();
+		}
+	}
+
+	synchronized private void changeColor() {
+		if ( ++colorIndex == someColors.length )
+			colorIndex = 0;
+		setForeground( currentColor() );
+		repaint();
+	}
+
+	synchronized private Color currentColor() {
+		return someColors[ colorIndex ];
+	}
+
+	public void run() {
+		while ( true ) {
+			blinkState = !blinkState;
+			repaint();
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+				// Handle error condition here ...
+			}
+		}
+	}
+
+	public void start() {
+		if ( blinkThread == null) {
+			blinkThread = new Thread(this);
+			blinkThread.start();
+		}
+	}
+
+	public void stop() {
+		if ( blinkThread != null) {
+			blinkThread.stop();
+			blinkThread = null;
+		}
+	}
+}
